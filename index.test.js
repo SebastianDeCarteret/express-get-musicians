815e9dab7e84f63fedbf43d9765439e07c24ae65
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-11-07 11:16:07 +0000

    part 1 done, now doing extensions

--- index.test.js
+++ index.test.js
@@ -1,22 +1,57 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const seedMusician = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
+const { syncSeed } = require("./seed");
+const { deserialize } = require("v8");
 
+describe("Musician API tests:", () => {
+  beforeEach(async () => {
+    await db.sync({ force: true });
+    await syncSeed();
+  });
+  describe("./musicians endpoint:", () => {
+    it("should return code: 200", async () => {
+      const response = await request(app).get("/musicians");
+      expect(response.statusCode).toBe(200);
+    });
+    it("should return code: 404", async () => {
+      await db.sync({ force: true });
+      const response = await request(app).get("/musicians");
+      expect(response.statusCode).toBe(404);
+    });
+    it("should return the correct values from the database compared to the seed", async () => {
+      const response = await request(app).get("/musicians");
+      response.body.forEach((object, index) => {
+        expect(object.name).toEqual(seedMusician[index].name);
+        expect(object.instrument).toEqual(seedMusician[index].instrument);
+      });
+    });
+  });
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+  describe("./musicians/:id endpoint:", () => {
+    it("should return code: 200", async () => {
+      const response = await request(app).get("/musicians/1");
+      expect(response.statusCode).toBe(200);
+    });
+    it("should return code: 404", async () => {
+      const response = await request(app).get("/musicians/100");
+      expect(response.statusCode).toBe(404);
+    });
+    it("should return the correct value from the database at each index compared to the seed", async () => {
+      seedMusician.forEach(async (object, index) => {
+        const response = await request(app).get(`/musicians/${index + 1}`);
+        expect(response.body.name).toEqual(seedMusician[index].name);
+        expect(response.body.instrument).toEqual(
+          seedMusician[index].instrument
+        );
+      });
+    });
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
-
-
-
-
-
-
+//TODO: Create a GET /musicians route to return all musicians
+app.get("/musicians", async (request, response) => {
+  const musicians = await Musician.findAll();
+  if (musicians.length === 0) {
+    response.status(404).send("Not found");
+    return;
+  }
+  response.json(musicians).status(200);
+});
+app.get("/musicians/:id", async (request, response) => {
+  const musician = await Musician.findByPk(request.params.id);
+  if (musician === null) {
+    response.status(404).send("Not found");
+    return;
+  }
+  response.json(musician).status(200);
+});
 
 module.exports = app;
