f6a885d008e9f2b54285cfe79504fdb4e03abd77
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-11-09 13:44:31 +0000

    added tests for checking the data validation works, updated all test names with their request type

--- index.test.js
+++ index.test.js
@@ -11,37 +11,59 @@ describe("Musician API tests:", () => {
   });
 
   describe("./musicians endpoint:", () => {
-    it("should return code: 200", async () => {
+    it("GET should return code: 200", async () => {
       const response = await request(app).get("/musicians");
       expect(response.statusCode).toBe(200);
     });
-    it("should return code: 200 when table has no values", async () => {
+    it("GET should return code: 200 when table has no values", async () => {
       await db.sync({ force: true });
       const response = await request(app).get("/musicians");
       expect(response.statusCode).toBe(200);
       expect(response.body.length).toBe(0);
     });
-    it("should return the correct values from the database compared to the seed", async () => {
+    it("GET should return the correct values from the database compared to the seed", async () => {
       const response = await request(app).get("/musicians");
       response.body.forEach((object, index) => {
         expect(object.name).toEqual(seedMusician[index].name);
         expect(object.instrument).toEqual(seedMusician[index].instrument);
       });
     });
+    it("POST should add a new resturant when keys are not empty", async () => {
+      const sendData = { name: "Japanese", instrument: "Bristol" };
+      const response = await request(app).post("/musicians").send(sendData);
+      const restaruantFound = await Musician.findByPk(4);
+      expect(restaruantFound.name).toEqual(sendData.name);
+      expect(restaruantFound.instrument).toEqual(sendData.instrument);
+      expect(response.body.error).toBe(undefined);
+    });
+    it("POST should return error when keys are empty", async () => {
+      const sentData = { name: "", instrument: "" };
+      const response = await request(app).post("/musicians").send(sentData);
+      response.body.error.forEach((error, index) => {
+        const errorMessageCurrent = {
+          location: "body",
+          path: Object.keys(sentData)[index], // get the key name at the index of loop
+          msg: "Invalid value",
+          type: "field",
+          value: "",
+        };
+        expect(error).toEqual(errorMessageCurrent);
+      });
+    });
   });
 
   describe("./musicians/:id endpoint:", () => {
-    it("should return code: 200", async () => {
+    it("GET should return code: 200", async () => {
       const response = await request(app).get("/musicians/1");
       expect(response.statusCode).toBe(200);
     });
-    it("should return code: 404", async () => {
+    it("GET should return code: 404", async () => {
       const response = await request(app).get("/musicians/100");
       expect(response.statusCode).toBe(404);
     });
 
     seedMusician.forEach((_, index) =>
-      it(`should return the correct value from the database for index: ${index} compared to the seed`, async () => {
+      it(`GET should return the correct value from the database for index: ${index} compared to the seed`, async () => {
         const response = await request(app).get(`/musicians/${index + 1}`);
         expect(response.body.name).toEqual(seedMusician[index].name);
         expect(response.body.instrument).toEqual(
@@ -52,18 +74,18 @@ describe("Musician API tests:", () => {
   });
 
   describe("./bands endpoint:", () => {
-    it("should return code: 200 when table has values", async () => {
+    it("GET should return code: 200 when table has values", async () => {
       const response = await request(app).get("/bands");
       expect(response.statusCode).toBe(200);
       expect(response.body.length).toBe(seedBand.length);
     });
-    it("should return code: 200 when table has no values", async () => {
+    it("GET should return code: 200 when table has no values", async () => {
       await db.sync({ force: true });
       const response = await request(app).get("/bands");
       expect(response.statusCode).toBe(200);
       expect(response.body.length).toBe(0);
     });
-    it("should return the correct values from the database compared to the seed", async () => {
+    it("GET should return the correct values from the database compared to the seed", async () => {
       const response = await request(app).get("/bands");
       response.body.forEach((object, index) => {
         expect(object.name).toEqual(seedBand[index].name);
